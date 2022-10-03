80979026c48f414db912135d64359a370553dd21
NesaDerivcoIpswich <nesa.ganesh@derivco.co.uk>
2022-10-03 12:44:37 +0100

    Made all the input as mandatory

--- src/components/pages/Diwali/DiwaliSports.js
+++ src/components/pages/Diwali/DiwaliSports.js
@@ -56,19 +56,19 @@ class DiwaliSports extends Component {
     }
 
     handleSubmit = (event) => {
-        alert(` Registered Successfully !!!!`)
-        
-        console.log(this.state);
-        event.preventDefault();
 
-        emailjs.sendForm('service_h6in7wb', 'template_hu6e9hm', event.target, 'WUNJawiU6bdr4-wXs')
-        .then((result) => {
-            console.log(result);
-        },
-        (error) => {
-            console.log(error);
-        }).catch(err => console.log(err));
+          alert(` Registered Successfully !!!!`)
+          console.log(this.state);
 
+          emailjs.sendForm('service_h6in7wb', 'template_hu6e9hm', event.target, 'WUNJawiU6bdr4-wXs')
+          .then((result) => {
+              console.log(result);
+          },
+          (error) => {
+              console.log(error);
+          }).catch(err => console.log(err));
+
+          event.preventDefault();
     }
 
     render() {
@@ -78,8 +78,8 @@ class DiwaliSports extends Component {
             <div>
 
                 <form onSubmit={this.handleSubmit}>
-                    <h1><u>Tornament for Mens and Womens</u></h1>
-                    <label>Sport :</label><select name="sport" defaultValue="Select Sport" onChange={(e) => this.handleChange(e)}>
+                    <h1><u>Tornament for Mens and Womens</u></h1><br /><br />
+                    <label>Sport * :</label><select name="sport" defaultValue="Select Sport" onChange={(e) => this.handleChange(e)}>
                         <option defaultValue>Select Sport</option>
                         <option value="cricket">Cricket</option>
                         <option value="badmiton">Badmiton</option>
@@ -95,25 +95,31 @@ class DiwaliSports extends Component {
                       <input value="family" name="challenge" type="radio" onChange={(e) => this.handleChange(e)}/>
                     </div>
                     <br />
-                    <br/>
-                    <label>FullName :</label> <input type="text" value={this.state.fullname} name="fullname"  placeholder="FullName..." /><br /><br />
-                    <label>Email Id :</label> <input type="text" value={this.state.emailid1} name="emailid1"  placeholder="EmailId..." /><br /><br />
-                    <label>Phone :</label> <input type="text" value={this.state.phone1} name="phone1"  placeholder="Phone..." /><br /><br />
-                    <label>Gender :</label>
+                    
+                    <label>FullName * :</label> <input type="text" value={this.state.fullname} name="fullname"  placeholder="FullName..." required /><br /><br />
+                    <label>Email Id * :</label> <input type="text" value={this.state.emailid1} name="emailid1"  placeholder="EmailId..." required /><br /><br />
+                    <label>Phone * :</label> <input type="text" value={this.state.phone1} name="phone1"  placeholder="Phone..." required /><br /><br />
+                    <label>Gender * :</label>
                     <select name="gender" defaultValue="Select Gender">
-                        <option defaultValue>Select Gender</option>
+                        <option defaultValue>Select Gender * </option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select><br /><br />
                     <div className={this.state.challenge}> 
-                    <label>Team Members/Partner Name :</label><br/>
+                    <label>Team Members/Partner Name  :</label><br/>
                     <textarea name='teamnames' rows="15" cols="50"></textarea>
-                    </div><br/><br />
-                    <label>Are you registerted in Diwali Nov 2022  </label>
-                    <input type="checkbox" id="diwaliReg" name="diwaliReg" value="Yes"></input>  <Link to='/diwali'>  (Diwali 2022 !!!!) </Link>
-                    
+                    </div>
+                    <br/><br />
+                    <label>Are you registerted in Diwali Nov 2022  * </label>
+                    <input type="checkbox" id="diwaliReg" name="diwaliReg" value="Yes" required></input>  <Link to='/diwali'>  (Diwali 2022 !!!!) </Link>
+                    <br/><br />
+                    <label>Have you received the details of the tournament  *  </label>
+                    <input type="checkbox" id="rules" name="rules" value="Yes" required></input>  
+                    <br/><label> Join this group to ask questions about the sports events <a href='https://chat.whatsapp.com/BiUri0P3IMaDw0zfBwa6AX'>WhatsAppGroup</a> </label>
                     <br /><br /> 
-                    <input type="submit" value="Submit" />
+                    <div className="submitClass">
+                      <input type="submit" value="Submit" />
+                    </div>
                     <br/><br/><br/>
                     
                 </form>
