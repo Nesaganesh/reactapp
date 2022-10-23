67ae72a82cef4596fe15c684158269a371260035
NesaDerivcoIpswich <nesa.ganesh@derivco.co.uk>
2022-10-23 20:08:12 +0100

    Diwali Reg Google Form

--- src/components/pages/Diwali/GoogleForm.js
+++ src/components/pages/Diwali/GoogleForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './GoogleForm.css';
-
+import '../../Button.css';
+import { Link } from 'react-router-dom';
 function GoogleForm() {
 
   return (
@@ -8,6 +9,17 @@ function GoogleForm() {
             <br/>
             <br/>
             <iframe title='googleForm' src="https://docs.google.com/forms/d/e/1FAIpQLSdzVcan3zo6EWqMks2VbpnhQZSUyZC2mDUu-CpFPUpsrst_LA/viewform?embedded=true" className='navbar-logo-diwali-att'>Loading…</iframe>
+            <br/>
+            <br/>
+              <Link to='/' className='btn-mobile'>
+                <button
+                  className='btns'
+                  buttonStyle='btn--primary'
+                  buttonSize='btn--large'
+                >
+                  Home Page
+                </button>
+              </Link>
             <br/>
             <br/>
     </>
