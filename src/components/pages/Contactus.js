d20470e4b48e424539f5567cb1c35b33d756a1bf
nesaganeshpanneerselvam <nesa246@gmail.com>
2024-02-14 08:53:39 +0000

    Classes and Videos updated

--- src/components/pages/Contactus.js
+++ src/components/pages/Contactus.js
@@ -23,7 +23,6 @@ function Contactus() {
                             <br/> Youtube: <a>https://www.youtube.com/@FlytoezDanceCompany</a><br/>
                         </div>                        
                     </div>
-                    <p><iframe width="560" height="315" src="https://www.youtube.com/embed/SOG1IYojcvE?si=J_zKpWJERf0l4wjm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
                 </div> 
         </form>
     </>
--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -8,7 +8,7 @@ function Videos() {
     <>
         <Navbar />
         <form className="form">
-            <div><h2>Contact Us</h2>
+            <div><h2>Trailer Annual Event 2023</h2>
                     
                     <p><iframe width="560" height="315" src="https://www.youtube.com/embed/SOG1IYojcvE?si=J_zKpWJERf0l4wjm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
                 </div> 
