db858f47ae49562ae431c9f1f702c5e60feeb717
nesaganeshpanneerselvam <nesa246@gmail.com>
2025-11-04 16:24:21 +0000

    Update contact information in Contactus component for accuracy and clarity.

--- src/components/pages/Contactus.js
+++ src/components/pages/Contactus.js
@@ -14,9 +14,9 @@ function Contactus() {
                             Please contact us with any questions or <br/>to book your place on one of our classes!
                         </div>
                         <div className="lastname">
-                        <br/>Admin Phone: 0776615266/07741843819/<br/>
-                        <br/>Admin WhatsApp: 0776615266/07424529361<br/>
-                        <br/>Email: flytoezdancecompany@gmail.com/flytoeza@gmail.com <br/>
+                        <br/>Phone: 07741843819<br/>
+                        <br/>WhatsApp: 07424529361<br/>
+                        <br/>Email: flytoezdancecompany@gmail.com<br/>
                         
                     
                             <br/>instagram: <a>https://www.instagram.com/flytoezdancecompany</a><br/>
